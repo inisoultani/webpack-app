5cd81d653a9d5adc9e13f2bef8d3a2a243e6c58d
Muhammad Soultani <muhammad.soultani@tiket.com>
2021-08-22 15:05:11 +0700

    7th

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -11,11 +11,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         // the order of the array is IMPORTANT, it loads BACKWARDS
         // so we need to make sure that css-loader (generate css in webpack)
         // then style-loader will inject the css into the DOM
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader", // 3. inject styles into DOM
+          "css-loader", // 2. turn css into commonJS
+          "sass-loader", // 1. turn scss into css
+        ],
       },
     ],
   },

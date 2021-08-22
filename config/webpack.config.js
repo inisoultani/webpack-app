0d7fb6164ac754132375c16605a9f7c2ac887068
Muhammad Soultani <muhammad.soultani@tiket.com>
2021-08-22 14:56:11 +0700

    6th

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,4 +8,15 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "../dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // the order of the array is IMPORTANT, it loads BACKWARDS
+        // so we need to make sure that css-loader (generate css in webpack)
+        // then style-loader will inject the css into the DOM
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };

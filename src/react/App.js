3c4f0b4156b907a85c66c2ebf89be8fc2f56cd4d
Muhammad Soultani <muhammad.soultani@tiket.com>
2021-08-24 01:30:32 +0700

    add styled-component sample

--- src/react/App.js
+++ src/react/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const App = () => {
-  return <div>This is React App</div>;
+  return <AppStyled>This is React App</AppStyled>;
 };
 
 export default App;
+
+const AppStyled = styled.div`
+  background-color: teal;
+  color: white;
+  margin: 10px 0;
+  padding: 10px 20px;
+`;
